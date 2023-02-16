49178dc6847e2b60c79ff18d760e210c89a08b49
Joshua Wakefield <43878645+jjwakefield@users.noreply.github.com>
2023-02-16 18:06:26 +0000

    Rearranged code

--- plot.py
+++ plot.py
@@ -4,6 +4,72 @@ from PyQt6 import QtWidgets, QtCore
 import random
 
 
+
+class PlotKalman(QtWidgets.QMainWindow):
+
+     def __init__(self, actual, measurements, filtered, x_range, y_range, update_interval, full_plot=False):
+          super(PlotKalman, self).__init__()
+
+          self.full_plot = full_plot
+          self.count = 0
+
+          self.graphWidget = pg.PlotWidget()
+          self.graphWidget.setXRange(x_range[0], x_range[1])
+          self.graphWidget.setYRange(y_range[0], y_range[1])
+          self.setCentralWidget(self.graphWidget)
+
+          self.actual = actual
+          self.measurements = measurements
+          self.filtered = filtered
+
+          self.actual_plot = np.array([[actual[0, 0], actual[0, 1]]])
+          self.measurement_plot = np.array([[measurements[0, 0], measurements[0, 1]]])
+          self.filtered_plot = np.array([[filtered[0, 0], filtered[0, 1]]])
+
+          self.graphWidget.setBackground('k')
+
+          self.graphWidget.addLegend(offset=(-1, 1))
+
+          self.measurement_scatter = self.graphWidget.plot(self.measurement_plot[:, 0], self.measurement_plot[:, 1], 
+                                                           pen=None, symbol='x', name='Measurement')
+          
+          self.filtered_line = self.graphWidget.plot(self.filtered_plot[:, 0], self.filtered_plot[:, 1], 
+                                                     pen=pg.mkPen('g', width=2), name='Filtered')
+          
+          self.actual_line =  self.graphWidget.plot(self.actual_plot[:, 0], self.actual_plot[:, 1], 
+                                                    pen=pg.mkPen('r', width=2), name='Actual')
+
+          self.timer = QtCore.QTimer()
+          self.timer.setInterval(update_interval)
+          self.timer.timeout.connect(self.update_plot_data)
+          self.timer.start()
+
+     def update_plot_data(self):
+          try:
+               self.actual = self.actual[1:, :]
+               self.filtered = self.filtered[1:, :]
+               self.measurements = self.measurements[1:, :]
+
+               self.actual_plot = np.concatenate((self.actual_plot, [self.actual[0, :]]))
+               self.filtered_plot = np.concatenate((self.filtered_plot, [self.filtered[0, :]]))
+               self.measurement_plot = np.concatenate((self.measurement_plot, [self.measurements[0, :]]))
+               
+               self.actual_line.setData(self.actual_plot[:, 0], self.actual_plot[:, 1])
+               self.filtered_line.setData(self.filtered_plot[:, 0], self.filtered_plot[:, 1])
+               self.measurement_scatter.setData(self.measurement_plot[:, 0], self.measurement_plot[:, 1])
+
+               if self.count >= 50 and self.full_plot == False:
+                    self.actual_plot = self.actual_plot[1:]
+                    self.filtered_plot = self.filtered_plot[1:]
+                    self.measurement_plot = self.measurement_plot[1:]
+               
+               self.count += 1
+
+          except:
+               pass
+
+
+
 class PlotParticleFilter(QtWidgets.QMainWindow):
 
      def __init__(self, actual, measurements, pf, particles, x_range, y_range, update_interval, full_plot):
@@ -81,75 +147,6 @@ class PlotParticleFilter(QtWidgets.QMainWindow):
 
                self.pf.reweight(self.measurements[-1, :])
                self.pf.resample()
-
-          except:
-               pass
-
-
-
-
-
-
-
-class PlotKalman(QtWidgets.QMainWindow):
-
-     def __init__(self, actual, measurements, filtered, x_range, y_range, update_interval, full_plot=False):
-          super(PlotKalman, self).__init__()
-
-          self.full_plot = full_plot
-          self.count = 0
-
-          self.graphWidget = pg.PlotWidget()
-          self.graphWidget.setXRange(x_range[0], x_range[1])
-          self.graphWidget.setYRange(y_range[0], y_range[1])
-          self.setCentralWidget(self.graphWidget)
-
-          self.actual = actual
-          self.measurements = measurements
-          self.filtered = filtered
-
-          self.actual_plot = np.array([[actual[0, 0], actual[0, 1]]])
-          self.measurement_plot = np.array([[measurements[0, 0], measurements[0, 1]]])
-          self.filtered_plot = np.array([[filtered[0, 0], filtered[0, 1]]])
-
-          self.graphWidget.setBackground('k')
-
-          self.graphWidget.addLegend(offset=(-1, 1))
-
-          self.measurement_scatter = self.graphWidget.plot(self.measurement_plot[:, 0], self.measurement_plot[:, 1], 
-                                                           pen=None, symbol='x', name='Measurement')
-          
-          self.filtered_line = self.graphWidget.plot(self.filtered_plot[:, 0], self.filtered_plot[:, 1], 
-                                                     pen=pg.mkPen('g', width=2), name='Filtered')
-          
-          self.actual_line =  self.graphWidget.plot(self.actual_plot[:, 0], self.actual_plot[:, 1], 
-                                                    pen=pg.mkPen('r', width=2), name='Actual')
-
-          self.timer = QtCore.QTimer()
-          self.timer.setInterval(update_interval)
-          self.timer.timeout.connect(self.update_plot_data)
-          self.timer.start()
-
-     def update_plot_data(self):
-          try:
-               self.actual = self.actual[1:, :]
-               self.filtered = self.filtered[1:, :]
-               self.measurements = self.measurements[1:, :]
-
-               self.actual_plot = np.concatenate((self.actual_plot, [self.actual[0, :]]))
-               self.filtered_plot = np.concatenate((self.filtered_plot, [self.filtered[0, :]]))
-               self.measurement_plot = np.concatenate((self.measurement_plot, [self.measurements[0, :]]))
-               
-               self.actual_line.setData(self.actual_plot[:, 0], self.actual_plot[:, 1])
-               self.filtered_line.setData(self.filtered_plot[:, 0], self.filtered_plot[:, 1])
-               self.measurement_scatter.setData(self.measurement_plot[:, 0], self.measurement_plot[:, 1])
-
-               if self.count >= 50 and self.full_plot == False:
-                    self.actual_plot = self.actual_plot[1:]
-                    self.filtered_plot = self.filtered_plot[1:]
-                    self.measurement_plot = self.measurement_plot[1:]
-               
-               self.count += 1
 
           except:
                pass
